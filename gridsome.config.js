6317aad2d73be857e14ebcd1cffa522510c3b18e
jake <jake101@gmail.com>
2019-08-12 00:21:02 -0700

    add netlify cms

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,7 +6,22 @@
 
 module.exports = {
   siteName: 'Grid',
-  plugins: [{
-    use: `gridsome-plugin-netlify-cms`
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'posts/**/*.md',
+        typeName: 'Post',
+        remark: {
+          plugins: [
+            // ...local plugins
+          ]
+        }
+      }
+    },
+    {
+    use: `gridsome-plugin-netlify-cms`,   options: {
+      publicPath: `/admin`
+    }
   } ]
 }
